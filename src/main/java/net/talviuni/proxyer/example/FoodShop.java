dfcc636c63d8c9b9d5bfa8520485797d57aab383
JannuMies <rantanen@talviuni.net>
2015-09-25 18:12:05 +0300

    FoodShop is just an interface, the example describing what to actually do is only left as a guideline commen.

--- src/main/java/net/talviuni/proxyer/example/FoodShop.java
+++ src/main/java/net/talviuni/proxyer/example/FoodShop.java
@@ -1,24 +1,22 @@
 package net.talviuni.proxyer.example;
 
-//public interface FoodShop {
-//    public Pastry getFood(Pound money);
+public interface FoodShop {
+    public Pastry getFood(Pound money);
     
 
-public class FoodShop {
-
-    
-    
-    Pastry getFood(Pound money) {
-        Euro euro = new Euro();
-        euro.setValue(money.getValue() * 0.8);
-        
-        StrudelShop strudelShop = new StrudelShop();
-        Strudel strudel = strudelShop.getFood(euro);
-        
-        Pastry pastry = new Pastry();
-        pastry.setName(strudel.getName());
-        pastry.setPrice(strudel.getPrice());
-        return pastry;
-    }
+//public class FoodShop {
+//    
+//    Pastry getFood(Pound money) {
+//        Euro euro = new Euro();
+//        euro.setValue(money.getValue() * 0.8);
+//        
+//        StrudelShop strudelShop = new StrudelShop();
+//        Strudel strudel = strudelShop.getFood(euro);
+//        
+//        Pastry pastry = new Pastry();
+//        pastry.setName(strudel.getName());
+//        pastry.setPrice(strudel.getPrice());
+//        return pastry;
+//    }
     
 }
